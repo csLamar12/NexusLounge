d95e76017998fb6139e632490a9632856f421907
csLamar12 <90802465+csLamar12@users.noreply.github.com>
2024-10-15 05:25:25 -0500

    Added Src/Resources/Drinks Added 20 png images labelled 1-20 to Drinks folder Added Java Doc Comments to: DataModel.java; LoginScreen.java; MainController.java;

--- src/Model/DataModel.java
+++ src/Model/DataModel.java
@@ -1,12 +1,16 @@
 package Model;
 
-import javax.swing.*;
-
 public class DataModel {
 
-
-    public boolean checkLogin(String username, String password) {
-        // Implement DB login logic
+    /**
+     * Delegate login check responsibilities to UserSQLProvider class
+     * @param username  User's Username
+     * @param password  User's Password
+     * @return  Returns a boolean value to represent success of authentication
+     *
+     */
+    public boolean authenticateUser(String username, String password) {
+        // Call UserSQLProvider to implement user authentication logic
         if (username.equals(" ") && password.equals(" ")) {
             return true;
         } else {
