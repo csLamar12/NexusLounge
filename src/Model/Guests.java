f404bb30209b8ea2a4bd00a6d91f7bb4b63ab668
abbygh19 <149645507+abbygh19@users.noreply.github.com>
2024-10-14 23:12:02 -0500

    Update Guests.java

--- src/Model/Guests.java
+++ src/Model/Guests.java
@@ -1,11 +1,13 @@
 import package Model;
 
+import java.util.Date;
+
 public class Guests extends Users
 {
   private Date dob;
 
   //Primary Constructor
-  public Guest(int id, String name, String username, String password, Date dob) 
+  public Guest(int id, String name, String username, String password, String role, Date dob) 
   {
     super(id, name, username, password, "Guest");
     this.dob = dob;
