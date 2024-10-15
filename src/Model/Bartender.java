2b4f7433ff01ae5c1f3b428b6bc0d6bedd8185cb
abbygh19 <149645507+abbygh19@users.noreply.github.com>
2024-10-14 23:07:01 -0500

    Create Bartender.java

---
+++ src/Model/Bartender.java
@@ -0,0 +1,9 @@
+import package Model;
+
+public class Bartender extends Users
+{
+  public Bartender(int id, String name, String username, String password, String role)
+  {
+    super(id, name, username, password, "Bartender");
+  }
+}
