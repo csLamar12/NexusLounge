023fe455a365109d3d158eaa59155ab83827e620
csLamar12 <90802465+csLamar12@users.noreply.github.com>
2024-10-16 03:14:44 -0500

    Edited User Class Added Child classes: Bartender.java; Guests.java; Manager.java;

--- src/Model/Users.java
+++ src/Model/Users.java
@@ -1,79 +1,109 @@
 package Model;
 
-public class Users
-{
-  protected int id;
-  protected String name;
-  protected String username;
-  protected String password;
-  protected String role;
 
-  //Primary Constructor
-  public Users(int id, String name, String username, String password, String role) 
-  {
-    this.id = id;
-    this.name = name;
-    this.username = username;
-    this.password = password;
-    this.role = role;
-  }
+/*
+Add methods and attributes as needed, JUST COMMUNICATE BEFORE DOING SO!!!
+ */
+public class Users {
+    protected int id;
+    protected String name;
+    protected String username;
+    protected String password;
+    protected String role;
 
-  //Getters
-  public int getId() 
-  {
-    return id;
-  }
+    // Default constructor
+    public Users() {
+      this.id = 0;
+      this.name = null;
+      this.username = null;
+      this.password = null;
+      this.role = null;
+    }
 
-  public String getName() 
-  {
-    return name;
-  }
+  /**
+   * Primary Controller for the Users Class
+   *
+   * @param id  Unique ID number for a user
+   * @param name  Name of the user (Nullable)
+   * @param username  User's username (Nullable)
+   * @param password  User's Password (Nullable)
+   * @param role  User's role (Guest, Manager, Bartender)
+   */
+    public Users(int id, String name, String username, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 
-  public String getUsername() 
-  {
-    return username;
-  }
-
-  public String getPassword() 
-  {
-    return password;
-  }
-
-  public String getRole() 
-  {
-    return role;
-  }
-
-  //Setters
-  public void setId(int id) 
-  {
-    this.id = id;
-  }
-
-  public void setName(String name) 
-  {
-    this.name = name;
-  }
-
-  public void setUsername(String username) 
-  {
-    this.username = username;
-  }
-
-  public void setPassword(String password) 
-  {
-    if(password.length() >= 8 && password.length() <= 12)
-    {
+  /**
+   * Primary Controller for the Users Class (Without ID field)
+   *
+   * @param name  Name of the user (Nullable)
+   * @param username  User's username (Nullable)
+   * @param password  User's Password (Nullable)
+   * @param role  User's role (Guest, Manager, Bartender)
+   */
+    public Users(String name, String username, String password, String role) {
+      this.name = name;
+      this.username = username;
       this.password = password;
+      this.role = role;
     }
-    else
-    {
-      throw new IllegalArgumentException("Password must be between 8 and 12 characters");
-    }
-  }
 
-  public void setRole(String role) 
-  {
-    this.role = role;
-  }
+    // Copy Constructor
+    public Users(Users copy) {
+        this.id = copy.id;
+        this.name = copy.name;
+        this.username = copy.username;
+        this.password = copy.password;
+        this.role = copy.role;
+    }
+
+    //Getters
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        if(password.length() >= 8 && password.length() <= 12){
+            this.password = password;
+        } else {
+            throw new IllegalArgumentException("Password must be between 8 and 12 characters");
+        }
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
 }
